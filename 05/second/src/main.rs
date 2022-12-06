803132f10b181c0687bfdeb1a82efd7def420293
Greg Yang <gregyang@zoho.com>
2022-12-05 21:04:24 -0800

    day 05

---
+++ 05/second/src/main.rs
@@ -0,0 +1,38 @@
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+    let mut stack : Vec<Vec<char>> = Vec::new();
+    stack.push("DHNQTWVB".chars().collect::<Vec<char>>());
+    stack.push("DWB".chars().collect::<Vec<char>>());
+    stack.push("TSQWJC".chars().collect::<Vec<char>>());
+    stack.push("FJRNZTP".chars().collect::<Vec<char>>());
+    stack.push("GPVJMST".chars().collect::<Vec<char>>());
+    stack.push("BWFTN".chars().collect::<Vec<char>>());
+    stack.push("BLDQFHVN".chars().collect::<Vec<char>>());
+    stack.push("HPFR".chars().collect::<Vec<char>>());
+    stack.push("ZSMBLNPH".chars().collect::<Vec<char>>());
+
+    for line in std::io::stdin().lines().map(|l| l.unwrap()) {
+        let caps_opt = re.captures(line.as_str());
+        if caps_opt.is_none() {
+            continue;
+        }
+
+        let caps = caps_opt.unwrap();
+        let count = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
+        let src = caps.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1;
+        let dst = caps.get(3).unwrap().as_str().parse::<usize>().unwrap() - 1;
+
+        for i in 0..count {
+            let ch = stack[src][stack[src].len() - count + i];
+            stack[dst].push(ch);
+        }
+
+        for _ in 0..count {
+            stack[src].pop();
+        }
+    }
+
+    println!("{}", stack.iter().map(|x| x[x.len() - 1]).collect::<String>());
+}
