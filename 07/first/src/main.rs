bc2c4e5c3ce1c5c26faa77e09e1a58a09f650aa3
Greg Yang <gregyang@zoho.com>
2022-12-08 22:22:37 -0800

    day 07

---
+++ 07/first/src/main.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+struct Node {
+    parent: usize,
+    size: i64,
+    children: HashMap<String, usize>,
+    files: HashSet<String>
+}
+
+impl Node {
+    fn new(parent: usize) -> Self {
+        Self {
+            parent,
+            size: 0,
+            children: HashMap::new(),
+            files: HashSet::new(),
+        }
+    }
+}
+
+fn main() {
+    let mut pool: Vec<Node> = Vec::new();
+    pool.push(Node::new(0));
+    let mut curr: usize = 0;
+
+    for line in std::io::stdin().lines().map(|l| l.unwrap()) {
+        let v: Vec<&str> = line.as_str().trim().split(' ').collect();
+        if v[0] == "$" {
+            if v[1] == "cd" {
+                if v[2] == ".." {
+                    curr = pool[curr].parent;
+                } else {
+                    match pool[curr].children.get(v[2]) {
+                        None => {
+                            pool.push(Node::new(curr));
+                            let cc = pool.len() - 1;
+
+                            pool[curr].children.insert(
+                                String::from(v[2]),
+                                cc);
+                            curr = cc;
+                        }
+                        Some(n) => curr = *n,
+                    }
+                }
+            }
+            continue;
+        }
+
+        if v[0] == "dir" {
+            continue;
+        }
+
+        let f = String::from(v[1]);
+        if pool[curr].files.contains(&f) {
+            continue;
+        }
+        pool[curr].files.insert(f);
+        let s = v[0].parse::<i64>().unwrap();
+        let mut p = curr;
+        while p != 0 {
+            pool[p].size += s;
+            p = pool[p].parent;
+        }
+    }
+
+    let mut count : i64 = 0;
+    for i in 0..pool.len() {
+        if pool[i].size <= 100000 {
+            count += pool[i].size;
+        }
+    }
+    println!("{}", count);
+}
